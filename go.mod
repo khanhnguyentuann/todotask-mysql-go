5dd6f6d00d2757457d25bb7d8b891ba361155155
khanhnguyentuann <khanh.nguyentuann@gmail.com>
2023-01-04 17:43:51 +0700

    update GetTask()

--- go.mod
+++ go.mod
@@ -5,20 +5,25 @@ go 1.19
 require (
 	github.com/astaxie/beego v1.12.3 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
-	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/prometheus/client_golang v1.7.0 // indirect
-	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.10.0 // indirect
-	github.com/prometheus/procfs v0.1.3 // indirect
-	github.com/shiena/ansicolor v0.0.0-20151119151921-a422bbe96644 // indirect
-	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
-	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
-	golang.org/x/sys v0.0.0-20200615200032-f1bc736245b1 // indirect
-	golang.org/x/text v0.3.0 // indirect
-	google.golang.org/protobuf v1.23.0 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.39.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"time"
@@ -99,6 +100,67 @@ func (c *TasksController) AddTask() {
 	c.ServeJSON()
 }
 
+// GetTasks retrieves a list of todo tasks for a specific user
+func (c *TasksController) GetTasks() {
+	// Extract the user ID from the URL parameters
+	var userID int
+	// Get the user ID string from the request parameters
+	userIDString := c.Ctx.Input.Param(":user_id")
+	if userIDString == "" {
+		c.CustomAbort(http.StatusBadRequest, "User id cannot be empty")
+	}
+	// Convert the user ID string to an integer
+	userID, err := strconv.Atoi(userIDString)
+	if err != nil {
+		// If the conversion fails, return a custom error response to the client
+		c.CustomAbort(http.StatusBadRequest, "Invalid user id")
+	}
+
+	// Create a new ORM object
+	o := orm.NewOrm()
+
+	// Get the list of tasks from the database
+	var tasks []TodoTask
+	_, err = o.QueryTable("todo_tasks").Filter("user_id", userID).All(&tasks)
+	if err != nil {
+		c.CustomAbort(http.StatusInternalServerError, "Error retrieving tasks")
+	}
+
+	// Return the list of tasks in the response body
+	c.Data["json"] = &tasks
+	c.ServeJSON()
+}
+
+// UpdateTask updates an existing todo task
+func UpdateTask(id int, task string) error {
+	o := orm.NewOrm()
+
+	todoTask := TodoTask{ID: id, Task: task}
+	_, err := o.Update(&todoTask)
+	if err == orm.ErrNoRows {
+		return fmt.Errorf("Task not found")
+	} else if err != nil {
+		return fmt.Errorf("Error updating task")
+	}
+
+	return nil
+}
+
+// DeleteTask deletes an existing todo task
+func DeleteTask(id int) error {
+	o := orm.NewOrm()
+
+	todoTask := TodoTask{ID: id}
+	_, err := o.Delete(&todoTask)
+	if err == orm.ErrNoRows {
+		return fmt.Errorf("Task not found")
+	} else if err != nil {
+		return fmt.Errorf("Error deleting task")
+	}
+
+	return nil
+}
+
 func main() {
 	//Register the MySQL driver with ORM.
 	orm.RegisterDriver("mysql", orm.DRMySQL)
@@ -115,6 +177,7 @@ func main() {
 
 	//Set up the router
 	beego.Router("/users/:user_id/tasks", &TasksController{}, "post:AddTask")
+	beego.Router("/users/:user_id/tasks", &TasksController{}, "get:GetTasks")
 
 	//Start the server
 	beego.Run()
