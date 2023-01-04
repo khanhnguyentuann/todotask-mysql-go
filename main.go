faa7eacdc6ff3e580fe4cda9555896d1e2be0ad1
khanhnguyentuann <khanh.nguyentuann@gmail.com>
2023-01-04 18:25:27 +0700

    update func GetTaskByID()

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 	"time"
@@ -42,6 +41,85 @@ func (u *User) TableName() string {
 // TasksController handles the API endpoints for managing todo tasks
 type TasksController struct {
 	beego.Controller
+}
+
+// GetTasks retrieves a list of todo tasks for a specific user
+func (c *TasksController) GetTasks() {
+	// Extract the user ID from the URL parameters
+	var userID int
+	// Get the user ID string from the request parameters
+	userIDString := c.Ctx.Input.Param(":user_id")
+	if userIDString == "" {
+		c.CustomAbort(http.StatusBadRequest, "User id cannot be empty")
+	}
+	// Convert the user ID string to an integer
+	userID, err := strconv.Atoi(userIDString)
+	if err != nil {
+		// If the conversion fails, return a custom error response to the client
+		c.CustomAbort(http.StatusBadRequest, "Invalid user id")
+	}
+
+	// Create a new ORM object
+	o := orm.NewOrm()
+
+	// Get the list of tasks from the database
+	var tasks []TodoTask
+	_, err = o.QueryTable("todo_tasks").Filter("user_id", userID).All(&tasks)
+	if err != nil {
+		c.CustomAbort(http.StatusInternalServerError, "Error retrieving tasks")
+	}
+
+	// Return the list of tasks in the response body
+	c.Data["json"] = &tasks
+	c.ServeJSON()
+}
+
+// GetTaskByID retrieves a specific todo task by its ID and checks if the current user has that task
+func (c *TasksController) GetTaskByID() {
+	// Extract the task ID and user ID from the URL parameters
+	var taskID, userID int
+	// Get the task ID and user ID strings from the request parameters
+	userIDString := c.Ctx.Input.Param(":user_id")
+	taskIDString := c.Ctx.Input.Param(":task_id")
+
+	if userIDString == "" {
+		c.CustomAbort(http.StatusBadRequest, "User id cannot be empty")
+	}
+	if taskIDString == "" {
+		c.CustomAbort(http.StatusBadRequest, "Task id cannot be empty")
+	}
+
+	// Convert the task ID and user ID strings to integers
+	userID, err := strconv.Atoi(userIDString)
+	taskID, err = strconv.Atoi(taskIDString)
+
+	if err != nil {
+		// If the conversion fails, return a custom error response to the client
+		c.CustomAbort(http.StatusBadRequest, "Invalid user id")
+	}
+	if err != nil {
+		// If the conversion fails, return a custom error response to the client
+		c.CustomAbort(http.StatusBadRequest, "Invalid task id")
+	}
+
+	// Get the task from the database
+	o := orm.NewOrm()
+	task := TodoTask{ID: taskID}
+	err = o.Read(&task)
+	if err == orm.ErrNoRows {
+		c.CustomAbort(http.StatusBadRequest, "Task not found")
+	} else if err != nil {
+		c.CustomAbort(http.StatusInternalServerError, "Error reading task")
+	}
+
+	// Check if the current user has the task
+	if task.UserID != userID {
+		c.CustomAbort(http.StatusForbidden, "You do not have permission to access this task")
+	}
+
+	// Return the task in the response body
+	c.Data["json"] = &task
+	c.ServeJSON()
 }
 
 // AddTask adds a new todo task
@@ -100,67 +178,6 @@ func (c *TasksController) AddTask() {
 	c.ServeJSON()
 }
 
-// GetTasks retrieves a list of todo tasks for a specific user
-func (c *TasksController) GetTasks() {
-	// Extract the user ID from the URL parameters
-	var userID int
-	// Get the user ID string from the request parameters
-	userIDString := c.Ctx.Input.Param(":user_id")
-	if userIDString == "" {
-		c.CustomAbort(http.StatusBadRequest, "User id cannot be empty")
-	}
-	// Convert the user ID string to an integer
-	userID, err := strconv.Atoi(userIDString)
-	if err != nil {
-		// If the conversion fails, return a custom error response to the client
-		c.CustomAbort(http.StatusBadRequest, "Invalid user id")
-	}
-
-	// Create a new ORM object
-	o := orm.NewOrm()
-
-	// Get the list of tasks from the database
-	var tasks []TodoTask
-	_, err = o.QueryTable("todo_tasks").Filter("user_id", userID).All(&tasks)
-	if err != nil {
-		c.CustomAbort(http.StatusInternalServerError, "Error retrieving tasks")
-	}
-
-	// Return the list of tasks in the response body
-	c.Data["json"] = &tasks
-	c.ServeJSON()
-}
-
-// UpdateTask updates an existing todo task
-func UpdateTask(id int, task string) error {
-	o := orm.NewOrm()
-
-	todoTask := TodoTask{ID: id, Task: task}
-	_, err := o.Update(&todoTask)
-	if err == orm.ErrNoRows {
-		return fmt.Errorf("Task not found")
-	} else if err != nil {
-		return fmt.Errorf("Error updating task")
-	}
-
-	return nil
-}
-
-// DeleteTask deletes an existing todo task
-func DeleteTask(id int) error {
-	o := orm.NewOrm()
-
-	todoTask := TodoTask{ID: id}
-	_, err := o.Delete(&todoTask)
-	if err == orm.ErrNoRows {
-		return fmt.Errorf("Task not found")
-	} else if err != nil {
-		return fmt.Errorf("Error deleting task")
-	}
-
-	return nil
-}
-
 func main() {
 	//Register the MySQL driver with ORM.
 	orm.RegisterDriver("mysql", orm.DRMySQL)
@@ -178,6 +195,7 @@ func main() {
 	//Set up the router
 	beego.Router("/users/:user_id/tasks", &TasksController{}, "post:AddTask")
 	beego.Router("/users/:user_id/tasks", &TasksController{}, "get:GetTasks")
+	beego.Router("/users/:user_id/tasks/:task_id", &TasksController{}, "get:GetTaskByID")
 
 	//Start the server
 	beego.Run()
